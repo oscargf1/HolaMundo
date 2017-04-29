c06445d500578770a42b1011fa3769f944b35074
oscargf1 <soporte.ifcol@gmail.com>
2017-04-28 21:55:37 -0500

    Initial commit

---
+++ app/src/main/java/com/example/oigarcia/holamundo/Hola_Mundo.java
@@ -0,0 +1,14 @@
+package com.example.oigarcia.holamundo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Hola_Mundo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hola__mundo);
+
+    }
+}
